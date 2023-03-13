8f91e0abfb8000dc5a400ec9b296a3ec6061d66a
LaurineDS <laurine.dasilva0@gmail.com>
2023-03-13 22:50:22 +0100

    add header and create defaultlayout to display both header and list

--- ornis/src/App.vue
+++ ornis/src/App.vue
@@ -1,18 +1,7 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
+import DefaultLayout from './layouts/DefaultLayout.vue';
 </script>
 
 <template>
-  <header></header>
-  <!--
-  <div class="wrapper">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </div>
--->
-  <RouterView />
+  <DefaultLayout />
 </template>
-
-<style scoped></style>
