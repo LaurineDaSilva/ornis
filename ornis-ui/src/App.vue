fe51bd1ac7cea35efdcd3245f5077d0088322042
LaurineDS <laurine.dasilva0@gmail.com>
2023-03-13 22:54:58 +0100

    refactor folder name to ornis-ui to prepare for backend folders

---
+++ ornis-ui/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import DefaultLayout from './layouts/DefaultLayout.vue';
+</script>
+
+<template>
+  <DefaultLayout />
+</template>
---
+++ ornis-ui/src/layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Header from '../components/commons/Header.vue';
+</script>
+
+<template>
+  <Header />
+  <main class="container-xl">
+    <RouterView />
+  </main>
+</template>
