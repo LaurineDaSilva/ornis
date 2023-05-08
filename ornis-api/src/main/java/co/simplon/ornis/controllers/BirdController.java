8bbaaa163e02b67eaede3de34316019a05600383
LaurineDS <laurine.dasilva0@gmail.com>
2023-05-08 19:02:00 +0200

    fix CORS

--- ornis-api/src/main/java/co/simplon/ornis/controllers/BirdController.java
+++ ornis-api/src/main/java/co/simplon/ornis/controllers/BirdController.java
@@ -2,7 +2,6 @@ package co.simplon.ornis.controllers;
 
 import java.util.Collection;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ import co.simplon.ornis.services.BirdService;
 
 @RestController
 @RequestMapping("/birds")
-@CrossOrigin
 public class BirdController {
 
     private final BirdService service;
