896dba42473d24057d63bd69630ccbbafac13bbc
LaurineDS <laurine.dasilva0@gmail.com>
2023-05-08 23:06:28 +0200

    updated local bird table

--- ornis-db/schema.ddl.sql
+++ ornis-db/schema.ddl.sql
@@ -3,6 +3,5 @@ DROP TABLE IF EXISTS birds;
 CREATE TABLE birds(
     id SERIAL PRIMARY KEY,
     vernacular_name VARCHAR(100) UNIQUE NOT NULL,
-    scientific_name VARCHAR(100) UNIQUE NOT NULL,
-    image_url VARCHAR(300)
+    scientific_name VARCHAR(100) UNIQUE NOT NULL
 );
