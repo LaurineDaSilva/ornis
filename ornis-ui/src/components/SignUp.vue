1c4bac0c28a8a66f3d9325c538384df3dd5360c0
LaurineDS <laurine.dasilva0@gmail.com>
2023-05-30 15:52:40 +0200

    [ORNIS-47] sign up component

---
+++ ornis-ui/src/components/SignUp.vue
@@ -0,0 +1,37 @@
+<script></script>
+
+<template>
+  <section>
+    <h1>{{ $t('signUp.title') }}</h1>
+    <form>
+      <div class="mb-3">
+        <label for="usernameInput" class="form-label"
+          >{{ $t('signUp.username.label') }}<span class="text-secondary">*</span></label
+        >
+        <input type="text" id="usernameInput" class="form-control" />
+        <div id="usernameInputHelpText" class="form-text">
+          {{ $t('signUp.username.helpText') }}
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="emailInput" class="form-label"
+          >{{ $t('signUp.email.label') }}<span class="text-secondary">*</span></label
+        >
+        <input type="email" id="emailInput" class="form-control" />
+        <div id="emailHelpText" class="form-text">{{ $t('signUp.email.helpText') }}</div>
+      </div>
+      <div class="mb-3">
+        <label for="passwordInput" class="form-label"
+          >{{ $t('signUp.password.label') }}<span class="text-secondary">*</span></label
+        >
+        <input type="password" id="passwordInput" class="form-control" />
+        <div id="passwordHelpText" class="form-text">
+          {{ $t('signUp.password.helpText') }} "<span class="fst-italic">%*!</span>".
+        </div>
+      </div>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <button type="submit" class="btn btn-primary">{{ $t('signUp.submit') }}</button>
+      </div>
+    </form>
+  </section>
+</template>
