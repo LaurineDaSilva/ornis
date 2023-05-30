e5e6811971a5c44529a0daddd9332d2f37f15c84
LaurineDS <laurine.dasilva0@gmail.com>
2023-05-30 12:11:22 +0200

    [ORNIS-12] add images to birds list

---
+++ ornis-ui/src/components/BirdsList.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  data() {
+    return {
+      baseUrl: import.meta.env.VITE_IMG_BASE_URL,
+      birds: [],
+    };
+  },
+  beforeMount() {
+    this.initBirdsList();
+  },
+  methods: {
+    async initBirdsList() {
+      const resp = await this.$http.get('/birds');
+      this.birds = await resp.body;
+    },
+  },
+};
+</script>
+
+<template>
+  <section>
+    <section>
+      <h1 class="mt-5 mb-4">{{ $t('birdsList.title') }}</h1>
+
+      <ul class="list-group">
+        <div v-for="bird in birds" :key="bird" class="card mb-3" style="max-width: 540px">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img
+                :src="'/images/bird_pictures/' + bird.speciesCode + '.jpg'"
+                class="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">{{ $t(`birdsCommonNames.${bird.speciesCode}`) }}</h5>
+                <p class="card-text fst-italic">
+                  {{ bird.scientificName }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ul>
+    </section>
+  </section>
+</template>
