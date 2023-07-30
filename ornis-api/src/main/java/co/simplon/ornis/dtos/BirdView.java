409635e4060ca751d7b1555867dc4291374d98c7
LaurineDS <laurine.dasilva0@gmail.com>
2023-07-30 18:52:10 +0200

    refacto: update bird entity and dto to get the french and english names of birds

--- ornis-api/src/main/java/co/simplon/ornis/dtos/BirdView.java
+++ ornis-api/src/main/java/co/simplon/ornis/dtos/BirdView.java
@@ -8,4 +8,8 @@ public interface BirdView {
 
     String getScientificName();
 
+    String getEnglishName();
+
+    String getFrenchName();
+
 }
--- ornis-api/src/main/java/co/simplon/ornis/entities/Bird.java
+++ ornis-api/src/main/java/co/simplon/ornis/entities/Bird.java
@@ -14,6 +14,12 @@ public class Bird extends AbstractEntity {
     @Column(name = "scientific_name")
     private String scientificName;
 
+    @Column(name = "english_name")
+    private String englishName;
+
+    @Column(name = "french_name")
+    private String frenchName;
+
     public Bird() {
     }
 
@@ -33,11 +39,28 @@ public class Bird extends AbstractEntity {
 	this.scientificName = scientificName;
     }
 
+    public String getEnglishName() {
+	return englishName;
+    }
+
+    public void setEnglishName(String englishName) {
+	this.englishName = englishName;
+    }
+
+    public String getFrenchName() {
+	return frenchName;
+    }
+
+    public void setFrenchName(String frenchName) {
+	this.frenchName = frenchName;
+    }
+
     @Override
     public String toString() {
 	return "{speciesCode=" + speciesCode
 		+ ", scientificName=" + scientificName
-		+ "}";
+		+ ", englishName=" + englishName
+		+ ", frenchName=" + frenchName + "}";
     }
 
 }
