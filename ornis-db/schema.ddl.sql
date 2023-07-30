8a918416aa9c290c02cdb463dac65c49654edeec
LaurineDS <laurine.dasilva0@gmail.com>
2023-07-30 18:29:18 +0200

    refacto: vernacular bird names in database instead of in json, for english and french

--- ornis-db/schema.ddl.sql
+++ ornis-db/schema.ddl.sql
@@ -3,5 +3,7 @@ DROP TABLE IF EXISTS birds;
 CREATE TABLE birds(
     id SERIAL PRIMARY KEY,
     species_code VARCHAR(100) UNIQUE NOT NULL,
-    scientific_name VARCHAR(100) UNIQUE NOT NULL
+    scientific_name VARCHAR(100) UNIQUE NOT NULL,
+    english_name VARCHAR(200) UNIQUE NOT NULL,
+    french_name VARCHAR(200) UNIQUE NOT NULL
 );
