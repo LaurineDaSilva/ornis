324980332711bdc51abee5eab250fcec0a96ccc8
LaurineDS <laurine.dasilva0@gmail.com>
2023-07-31 20:56:26 +0200

    refacto: create tables french_messages and english_messages for translation

--- ornis-db/schema.ddl.sql
+++ ornis-db/schema.ddl.sql
@@ -1,9 +1,21 @@
 DROP TABLE IF EXISTS birds;
+DROP TABLE IF EXISTS english_messages;
+DROP TABLE IF EXISTS french_messages;
 
-CREATE TABLE birds(
+CREATE TABLE english_messages (
     id SERIAL PRIMARY KEY,
     species_code VARCHAR(100) UNIQUE NOT NULL,
-    scientific_name VARCHAR(100) UNIQUE NOT NULL,
-    english_name VARCHAR(200) UNIQUE NOT NULL,
+    english_name VARCHAR(200) UNIQUE NOT NULL
+);
+
+CREATE TABLE french_messages (
+    id SERIAL PRIMARY KEY,
+    species_code VARCHAR(100) UNIQUE NOT NULL,
     french_name VARCHAR(200) UNIQUE NOT NULL
+);
+
+CREATE TABLE birds (
+    id SERIAL PRIMARY KEY,
+    species_code VARCHAR(100) UNIQUE NOT NULL,
+    scientific_name VARCHAR(100) UNIQUE NOT NULL
 );
