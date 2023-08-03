c7a7fc61d23472165e1acfe03df02642dc9f3081
LaurineDS <laurine.dasilva0@gmail.com>
2023-08-03 22:22:54 +0200

    refacto: style birds list into two columns

--- ornis-ui/src/components/BirdsList.vue
+++ ornis-ui/src/components/BirdsList.vue
@@ -22,21 +22,30 @@ export default {
   <section>
     <h1 class="mt-5 mb-4">{{ $t('birdsList.title') }}</h1>
     <ul class="list-group">
-      <div v-for="bird in birds" :key="bird" class="card mb-3 mx-auto" style="max-width: 540px">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img
-              :src="`/images/bird_pictures/${bird.speciesCode}.jpg`"
-              class="img-fluid rounded-start"
-              :alt="$t('birdsList.imageAlt', { name: bird.commonName })"
-            />
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">{{ bird.commonName }}</h5>
-              <p class="card-text fst-italic">
-                {{ bird.scientificName }}
-              </p>
+      <div class="row">
+        <div
+          v-for="bird in birds"
+          :key="bird"
+          class="col-md-6 mb-4 mx-auto"
+          style="max-width: 540px"
+        >
+          <div class="card h-100 shadow-sm">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img
+                  :src="`/images/bird_pictures/${bird.speciesCode}.jpg`"
+                  class="img-fluid h-100 rounded-start"
+                  :alt="$t('birdsList.imageAlt', { name: bird.commonName })"
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">{{ bird.commonName }}</h5>
+                  <p class="card-text fst-italic">
+                    {{ bird.scientificName }}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
