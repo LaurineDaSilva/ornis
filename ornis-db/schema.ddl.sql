c2a813623a8199193594e3fd07407465df781f37
LaurineDS <laurine.dasilva0@gmail.com>
2023-08-20 15:58:17 +0200

    refacto: add user_accounts table to schema.ddl.sql

--- ornis-db/schema.ddl.sql
+++ ornis-db/schema.ddl.sql
@@ -3,6 +3,7 @@ SET CLIENT_ENCODING TO 'UTF-8';
 DROP TABLE IF EXISTS birds;
 DROP TABLE IF EXISTS english_messages;
 DROP TABLE IF EXISTS french_messages;
+DROP TABLE IF EXISTS user_accounts;
 
 CREATE TABLE english_messages (
     id SERIAL PRIMARY KEY,
@@ -21,4 +22,12 @@ CREATE TABLE birds (
     species_code VARCHAR(100) UNIQUE NOT NULL,
     scientific_name VARCHAR(100) UNIQUE NOT NULL,
     common_name VARCHAR(200)
+);
+
+CREATE TABLE user_accounts (
+    id SERIAL PRIMARY KEY, 
+    email_address VARCHAR(50), 
+    username VARCHAR(10), 
+    password VARCHAR(30), 
+    roles_id INTEGER
 );
